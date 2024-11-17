623f0471a19c3e669ae46da69b2b8a9ae1728e84
Chris Pring <chrisjlpring@outlook.com>
2024-11-17 14:40:49 +0000

    Configure deployment to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
+// Replace '<repository-name>' with your actual repository name, e.g., 'simpliquote'
+const repositoryName = "SimpliQuote";
+
 export default defineConfig({
   plugins: [react()],
+  base: `/${SimpliQuote}/`, // Set the base path for GitHub Pages
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./src/test/setup.js", // Updated path here
+    setupFiles: "./src/test/setup.js", // Ensure this path is correct
     include: ["src/tests/**/*.{test,spec}.{js,jsx,ts,tsx}"],
     exclude: ["node_modules", "dist", ".idea", ".git", ".cache"],
   },
